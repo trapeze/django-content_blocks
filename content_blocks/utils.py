49baeff60b1d2cbc3b115badd4ed6dce8ea9aef6
sbull <sbull@2210ab8a-7944-0410-bba6-939b3ad99faa>
2010-02-10 15:27:38 +0000

    content_blocks: Replaced dependence on test_helper with explicit admin URL lookup.

--- content_blocks/utils.py
+++ content_blocks/utils.py
@@ -1,16 +1,22 @@
+from django.contrib import admin
 from django.core.urlresolvers import reverse
-from test_helper.utils import TestHelper
 
 
 def get_admin_list_page(model):
     """
     Returns the admin list page path for the given model.
     """
-    return TestHelper().getAdminListPage(model)
+    app_label = model._meta.app_label
+    model_name = model.__name__
+    return reverse('admin:%s_%s_changelist' % (app_label, model_name.lower()))
 
 
 def get_admin_edit_page(instance):
     """
     Returns the admin edit page path for the given instance.
     """
-    return TestHelper().getAdminEditPage(instance)
+    app_label = instance.__class__._meta.app_label
+    model_name = instance.__class__.__name__
+    return reverse(
+            'admin:%s_%s_change' % (app_label, model_name.lower()),
+            args=(instance.pk,))
