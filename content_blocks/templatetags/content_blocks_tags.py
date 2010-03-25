0f627988eeddb7b63be7fe2c70fbef92ef8d72ed
dlimeback <dlimeback@2210ab8a-7944-0410-bba6-939b3ad99faa>
2010-03-25 15:40:52 +0000

    content_blocks: Added ability to specify amount of lorem text.

--- content_blocks/templatetags/content_blocks_tags.py
+++ content_blocks/templatetags/content_blocks_tags.py
@@ -9,7 +9,7 @@ register = template.Library()
 
 
 @register.inclusion_tag("content_blocks/block.html", takes_context=True)
-def show_content_block(context, name, editable=True, markup=True):
+def show_content_block(context, name, editable=True, markup=True, amount=''):
     """
     Renders the block with the given name and the context language
     If there block has no content and DEBUG is true, renders lorem ipsum text
@@ -22,12 +22,18 @@ def show_content_block(context, name, editable=True, markup=True):
     except ContentBlock.DoesNotExist:
         block = None
 
+    try:
+        amount = int(amount)
+    except ValueError:
+        amount = None
+
     return {
         "name": name,
         "block": block,
         "editable": (editable is True or editable == "True"),
         "markup": (markup is True or markup == "True"),
         "perms": context.get("perms", None),
+        "amount": amount,
         "LANGUAGE_CODE": context.get("LANGUAGE_CODE", None),
         "MEDIA_URL": context.get("MEDIA_URL", None),
         "DEBUG": settings.DEBUG,
