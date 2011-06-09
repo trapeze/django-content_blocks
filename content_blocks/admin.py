0f3ef6c62a156f0257110557e219521245299e54
klee <klee@2210ab8a-7944-0410-bba6-939b3ad99faa>
2011-06-09 19:39:38 +0000

    content_blocks: Ability to export a selected list of content blocks to a json file. http://ticket.trapeze.com/app/item/R001011-179/

--- content_blocks/admin.py
+++ content_blocks/admin.py
@@ -1,11 +1,29 @@
+import datetime
+
 from django.contrib import admin
+from django.contrib.contenttypes.models import ContentType
+from django.core import serializers
+from django.http import HttpResponse
 
 from content_blocks.models import ContentBlock, ImageBlock
 
 
 class ContentBlockAdmin(admin.ModelAdmin):
     list_display = ('name','modification_date',)
-
+    actions = ['export_selected_objects']
+    
+    def export_selected_objects(self, request, queryset):
+        selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
+        ct = ContentType.objects.get_for_model(queryset.model)
+        response = HttpResponse(mimetype="text/javascript")
+        response['Content-Disposition'] = u'attachment; filename=contentblocks_%s' % \
+        (datetime.datetime.now().strftime('%Y%m%d%H%M%S'))
+        
+        serializers.serialize("json", queryset, stream=response)
+    
+        return response
+    export_selected_objects.short_description = "Export to JSON"
+    
 admin.site.register(ContentBlock, ContentBlockAdmin)
 
 admin.site.register(ImageBlock)
